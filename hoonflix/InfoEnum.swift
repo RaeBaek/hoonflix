61ac62a8b8c324cb572d8737fd126391d3f8fb1c
RaeBaek <bmp117@naver.com>
2023-07-26 22:21:45 +0900

    SeSAC 2주 3일차 (230726 .Wed)
    1. 텍스트 필드의 태그 값을 열거형을 사용하여 보다 명확한 값으로 접근
    2. 로그인 버튼 탭 시 조건을 따진 후 적합한 상황에 다음 화면으로 넘어가는 push 작업 처리
    3. 회원가입 버튼 탭 시 조건을 따진 후 부족한 상황에는 Alert 알림 창을 띄우는 작업 처리
    4. 초기화 버튼 탭 시 UserDefaults에 저장된 모든 정보를 제거하는 작업 처리

---
+++ hoonflix/InfoEnum.swift
@@ -0,0 +1,17 @@
+//
+//  InfoEnum.swift
+//  hoonflix
+//
+//  Created by 백래훈 on 2023/07/26.
+//
+
+import Foundation
+
+enum TextFieldType: Int, CaseIterable {
+    case email = 0
+    case password = 1
+    case nickname = 2
+    case location = 3
+    case recommandCode = 4
+    
+}
--- hoonflix/SignUpViewController.swift
+++ hoonflix/SignUpViewController.swift
@@ -7,40 +7,35 @@
 
 import UIKit
 
-enum TextFieldType: Int {
-    case email = 100
-    case password = 200
-    case nickname = 300
-    case location = 400
-    case recommandCode = 500
-}
-
 class SignUpViewController: UIViewController {
     
-    @IBOutlet var emailOrPhoneNumber: UITextField!
-    @IBOutlet var password: UITextField!
-    @IBOutlet var nickname: UITextField!
-    @IBOutlet var location: UITextField!
-    @IBOutlet var recommandCode: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var nicknameTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var recommandCodeTextField: UITextField!
+    
+    @IBOutlet var loginButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
+    
     @IBOutlet var hoonflixSwitch: UISwitch!
+    @IBOutlet var clearButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        emailOrPhoneNumber.tag = TextFieldType.email.rawValue
-        password.tag = TextFieldType.password.rawValue
-        nickname.tag = TextFieldType.nickname.rawValue
-        location.tag = TextFieldType.location.rawValue
-        recommandCode.tag = TextFieldType.recommandCode.rawValue
+        // textField 디자인
+        designTextField(name: emailTextField, text: "이메일", keyboardType: .emailAddress, isSecureTextEntry: false)
+        designTextField(name: passwordTextField, text: "비밀번호", keyboardType: .default, isSecureTextEntry: true)
+        designTextField(name: nicknameTextField, text: "닉네임", keyboardType: .default, isSecureTextEntry: false)
+        designTextField(name: locationTextField, text: "위치", keyboardType: .default, isSecureTextEntry: false)
+        designTextField(name: recommandCodeTextField, text: "추천 코드 입력", keyboardType: .default, isSecureTextEntry: false)
         
-        designTextField(name: emailOrPhoneNumber, text: "이메일", keyboardType: .emailAddress, isSecureTextEntry: false)
-        designTextField(name: password, text: "비밀번호", keyboardType: .default, isSecureTextEntry: true)
-        designTextField(name: nickname, text: "닉네임", keyboardType: .default, isSecureTextEntry: false)
-        designTextField(name: location, text: "위치", keyboardType: .default, isSecureTextEntry: false)
-        designTextField(name: recommandCode, text: "추천 코드 입력", keyboardType: .default, isSecureTextEntry: false)
-        
+        setTextFieldTag()
+        setTextFieldText()
+        designLoginButton()
         designSignUpButton()
+        designClearButton()
         designHoonflixSwitch()
         
     }
@@ -96,12 +91,48 @@ class SignUpViewController: UIViewController {
         
     }
     
-    @IBAction func signUpButtonTapped(_ sender: UIButton) {
-        view.endEditing(true)
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        if (emailTextField.text?.isEmpty == false && passwordTextField.text?.isEmpty == false && nicknameTextField.text?.isEmpty == false && locationTextField.text?.isEmpty == false) {
+            guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "MovieViewController") as? MovieViewController else { return }
+                    self.navigationController?.pushViewController(nextVC, animated: true)
+        } else {
+            signUpWarningAlert()
+        }
     }
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        if (emailTextField.text?.isEmpty == false && passwordTextField.text?.isEmpty == false && nicknameTextField.text?.isEmpty == false && locationTextField.text?.isEmpty == false) {
+            UserDefaults.standard.set(emailTextField.text, forKey: "email")
+            UserDefaults.standard.set(passwordTextField.text, forKey: "password")
+            UserDefaults.standard.set(nicknameTextField.text, forKey: "nickname")
+            UserDefaults.standard.set(locationTextField.text, forKey: "location")
+            UserDefaults.standard.set(recommandCodeTextField.text, forKey: "recommandCode")
+            
+            view.endEditing(true)
+        } else {
+            signUpWarningAlert()
+        }
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+        clearWarningAlert()
+    }
+    
+//    @IBAction func saveButtonClicked(_ sender: UIButton) {
+//        // 저장 버튼 클릭 횟수 저장 기능
+//        // 1. 저장된 횟수 가지고 오기
+//        let value = UserDefaults.standard.integer(forKey: "count")
+//        // 2. 저장된 홧수에 1을 더하기
+//        let result = value + 1
+//        // 3. 2번에서 나온 결과를 다시 저장해주기
+//        UserDefaults.standard.set(result, forKey: "count")
+//        // 4. 확인
+//        print(UserDefaults.standard.integer(forKey: "count"))
+//    }
     
     @IBAction func tapGestureTapped(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
+        
     }
     
     // 5개의 텍스트 필드를 하나의 함수를 이용하여 디자인
@@ -119,12 +150,22 @@ class SignUpViewController: UIViewController {
         
     }
     
-    // 회원가입 버튼은 1개이므로 매개변수 없이 직접 구현
+    func designLoginButton() {
+        loginButton.setTitle("로그인", for: .normal)
+        loginButton.setTitleColor(.black, for: .normal)
+        loginButton.backgroundColor = .white
+        loginButton.clipsToBounds = true
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.borderColor = UIColor.black.cgColor
+        loginButton.layer.borderWidth = 1
+    }
+    
     func designSignUpButton() {
-        signUpButton.titleLabel?.font = .systemFont(ofSize: 10, weight: .bold)
         signUpButton.setTitle("회원가입", for: .normal)
         signUpButton.setTitleColor(.white, for: .normal)
         signUpButton.backgroundColor = .black
+        signUpButton.clipsToBounds = true
+        signUpButton.layer.cornerRadius = 5
         signUpButton.layer.borderColor = UIColor.white.cgColor
         signUpButton.layer.borderWidth = 1
     }
@@ -134,5 +175,79 @@ class SignUpViewController: UIViewController {
         hoonflixSwitch.setOn(false, animated: true)
         hoonflixSwitch.onTintColor = .red
         hoonflixSwitch.thumbTintColor = .white
+    }
+    
+    func designClearButton() {
+        clearButton.setTitle("초기화", for: .normal)
+        clearButton.setTitleColor(.black, for: .normal)
+        clearButton.backgroundColor = .white
+        clearButton.clipsToBounds = true
+        clearButton.layer.cornerRadius = 5
+    }
+    
+    func setTextFieldTag() {
+        // 각 textField에 tag 값 부여
+        emailTextField.tag = TextFieldType.email.rawValue
+        passwordTextField.tag = TextFieldType.password.rawValue
+        nicknameTextField.tag = TextFieldType.nickname.rawValue
+        locationTextField.tag = TextFieldType.location.rawValue
+        recommandCodeTextField.tag = TextFieldType.recommandCode.rawValue
+    }
+    
+    func setTextFieldText() {
+        // UserDefaults를 이용해 저장된 값 가져오기
+        let email = UserDefaults.standard.string(forKey: "email")
+        let password = UserDefaults.standard.string(forKey: "password")
+        let nickname = UserDefaults.standard.string(forKey: "nickname")
+        let location = UserDefaults.standard.string(forKey: "location")
+        let recommandCode = UserDefaults.standard.string(forKey: "recommandCode")
+        
+        // 가져온 값을 텍스트필드에 표현
+        if let email {
+            emailTextField.text = email
+        }
+        if let password {
+            passwordTextField.text = password
+        }
+        if let nickname {
+            nicknameTextField.text = nickname
+        }
+        if let location {
+            locationTextField.text = location
+        }
+        if let recommandCode {
+            recommandCodeTextField.text = recommandCode
+        }
+    }
+    
+    func signUpWarningAlert() {
+        let alert = UIAlertController(title: "확인하세요!", message: "추천코드를 제외한 나머지 항목들은 필수로 기입해주세요.", preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        present(alert, animated: true)
+        
+    }
+    
+    func clearWarningAlert() {
+        let alert = UIAlertController(title: "주의하세요!", message: "한 번 초기화를 하시면 돌이킬 수 없습니다. 확인하셨습니까?", preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .destructive)
+        let ok = UIAlertAction(title: "확인", style: .default, handler: { [self] _ in resetUserDefaultsValue() })
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        present(alert, animated: true)
+    }
+    
+    func resetUserDefaultsValue() {
+        for key in UserDefaults.standard.dictionaryRepresentation().keys {
+            UserDefaults.standard.removeObject(forKey: key)
+        }
+        setTextFieldText()
     }
 }
